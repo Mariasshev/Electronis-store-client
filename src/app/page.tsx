0e32b5caa2e49d0ec09b5bc1bbc936fe31861f69
Mariasshev <sevcenkomaria72@gmail.com>
2026-01-03 23:43:20 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import BootstrapProvider from "../components/BootstrapProvider";
 import Hero from "../components/Hero";
 import BrowseCategories from "../components/BrowseCategories";
+import Discount from "../components/Discount";
+import Banner from "../components/Banner";
+import ProductsTabs from "../components/ProductsTabs";
+import SaleBanner from "../components/SaleBanner";
+
 
 export default function Home() {
-    return (
-        <BootstrapProvider>
-
-            <main>
-                <Hero />
-                <BrowseCategories />
-            </main>
-
-
-        </BootstrapProvider>
-    );
+  return (
+    <>
+      <Hero />
+      <BrowseCategories />
+      <ProductsTabs />
+      <Banner />
+      <Discount />
+      <SaleBanner />
+    </>
+  );
 }
